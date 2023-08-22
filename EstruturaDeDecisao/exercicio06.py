868a0cf02eeb5ba8263990600faf2338c7d49835
Mirella Leoni <109217533+miiswan@users.noreply.github.com>
2023-08-22 19:12:39 -0300

    Update exercicio06.py

--- EstruturaDeDecisao/exercicio06.py
+++ EstruturaDeDecisao/exercicio06.py
@@ -8,5 +8,7 @@ if (num1 > num2 and num1 > num3):
     print(f"O número {num1} é o maior entre eles.")
 elif (num2 > num1 and num2 > num3):
     print(f"O número {num2} é o maior entre eles.")
-else:
+elif (num3 > num1 and num3 > num2):
     print(f"O número {num3} é o maior entre eles.")
+else:
+    print(f"Você inseriu dois ou mais números iguais.")
